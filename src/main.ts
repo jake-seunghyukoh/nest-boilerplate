7f16f1cb257ee829b5eb7c43da242395fb790c19
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-03 13:49:04 +0900

    Fix - Csrf issue - Cookie parser and session should be initialized before csurf

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,29 @@ import { AppModule } from './app.module';
 
 import * as compression from 'compression';
 import * as helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
+import * as session from 'express-session';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  app.use(csurf());
+
   app.use(compression());
   app.use(helmet());
-  app.use(csurf());
-  app.enableCors();
 
   await app.listen(3000);
 }
