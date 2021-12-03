02f5dbc6aafe59b6339ace75959dc36058ff1bba
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-03 13:17:25 +0900

    SetUp - Csurf

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 
 import * as compression from 'compression';
 import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(compression());
   app.use(helmet());
+  app.use(csurf());
   app.enableCors();
 
   await app.listen(3000);
