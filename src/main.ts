62a31e4af720072d4451a424767e26de26c30d6b
Seunghyuk <seunghyukoh0@gmail.com>
2021-12-03 13:15:31 +0900

    SetUp - Helmet

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import * as compression from 'compression';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(compression());
+  app.use(helmet());
 
   await app.listen(3000);
 }
